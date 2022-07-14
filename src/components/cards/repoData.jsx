dd40a414ea2ef3976a6db720b44e1fd29a6ff956
Huy Vo <vohuy819@gmail.com>
2022-07-14 14:12:56 +0930

    add lastest project

--- src/components/cards/repoData.jsx
+++ src/components/cards/repoData.jsx
@@ -1,11 +1,15 @@
 import guru from './img/guru.png';
 import tech_blog from './img/Tech_blog.png';
-import socialNet from './img/socialNet.png';
 import cocktail from './img/cocktail.png'
-import weather from './img/WeatherDashboard.png';
-import ecommerce from './img/eCommerce.png'
+import trek from './img/trek.png';
 
 export const repos = [
+    {
+    "name":"Trek-photo sharing social network",
+    "github":"https://github.com/HuyHHV/trek",
+    "link": "https://trekau-app.herokuapp.com/",
+    "screenshot" : trek
+    },
     {
     "name":"Guru-Project managment app",
     "github":"https://github.com/AshWeston/PROJECT2",
@@ -19,27 +23,9 @@ export const repos = [
     "screenshot" : tech_blog
     },
     {
-    "name":"Social Network API",
-    "github":"https://github.com/HuyHHV/Social_Network_API",
-    "link": "https://github.com/HuyHHV/Social_Network_API",
-    "screenshot" : socialNet
-    },
-    {
     "name":"Cocktail Bar Stocktake App",
     "github":"https://github.com/lconnell1000/Project-1",
     "link": "https://lconnell1000.github.io/Project-1/",
     "screenshot" : cocktail
-    },
-    {
-    "name":"Weather Dashboard",
-    "github":"https://github.com/HuyHHV/Weather_Dashboard",
-    "link": "https://huyhhv.github.io/Weather_Dashboard/",
-    "screenshot" : weather
-    },
-    {
-    "name":"E-Commerce Back-End",
-    "github":"https://github.com/HuyHHV/E-Commerce_Back_End",
-    "link": "https://drive.google.com/file/d/1OX0-r9SO-DdNF_ZzeQigcWcq2Iryrp5l/view",
-    "screenshot" :ecommerce
     }
 ]
